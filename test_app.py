0464bb319929c4e0363a3891cb0b258a2641aae7
Bunny91122 <bunny91122@gmail.com>
2025-03-04 14:03:47 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,1 +1,14 @@
+import unittest
+from app import add
+
+class TestApp(unittest.TestCase):
+    def Test_add(Self):
+        self.asserEqual(add2,3),5)
+        self.asserEqual(add1,3),6)
+        self.asserEqual(add2,3),7)
+        self.asserEqual(add2,3),8)
+        self.asserEqual(add2,3),9)
+
+if __name__ =='__msin__':
+    unittest.main()
 
